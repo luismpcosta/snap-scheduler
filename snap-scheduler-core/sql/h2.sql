f3bbc4b0aa3a8895b1c5d5640cd38b5c018f9e7a
Luis Costa <luis.manuel.costa@credibom.pt>
2021-01-02 22:08:24 +0000

    h2 schema

---
+++ snap-scheduler-core/sql/h2.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS snap_lock
+(
+    key varchar(255) NOT NULL,
+	method varchar(255) NOT NULL,
+    lock_until datetime NOT NULL,
+    lock_at datetime NOT NULL,
+    lock_by text NOT NULL,
+    PRIMARY KEY (key, method)
+);
+
+CREATE TABLE IF NOT EXISTS snap_task_audit
+(
+	id BIGINT AUTO_INCREMENT NOT NULL,
+    key varchar(100) NOT NULL,
+  	method varchar(255) NOT NULL,
+	run_on varchar(100) NOT NULL,
+    start_run datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    end_run datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    run_time_seconds BIGINT NOT NULL,
+    task_error text,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS snap_scheduler (
+	name text NOT NULL,
+	key varchar(255) NOT NULL,
+	type text NOT NULL,
+	task_class text NOT NULL,
+	task_data text,
+	task_data_class text,
+	run_at datetime NOT NULL,
+	recurrence text,
+	picked boolean NOT NULL DEFAULT false,
+	picked_by text,
+	end_run datetime,
+	PRIMARY KEY (key)
+);
